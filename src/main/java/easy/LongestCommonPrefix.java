29cb49e0e241fa3235f4fb49b75ae11cd690a92b
miguelsalto <miguel.salto@gmail.com>
2020-06-02 00:42:47 -0700

    Simplify implementation of LongestCommonPrefix

--- src/main/java/easy/LongestCommonPrefix.java
+++ src/main/java/easy/LongestCommonPrefix.java
@@ -2,54 +2,27 @@ package easy;
 
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
+        int nStrs = strs.length;
+        if (nStrs == 0) {
             return "";
         }
-        Trie trie = new Trie();
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < strs.length; ++i) {
-            String s = strs[i];
-            min = Math.min(min, trie.insert(s, i));
-            if (min == 0) {
-                return "";
+        String minStr = strs[0];
+        for (int i = 1; i < nStrs; ++i) {
+            if (strs[i].length() < minStr.length()) {
+                minStr = strs[i];
             }
         }
-        return strs[0].substring(0, min);
-    }
-
-    private static class Trie {
-        TrieNode root = new TrieNode(' ');
-
-        int insert(String s, int matches) {
-            int sLength = s.length();
-            TrieNode current = root;
-            int count = 0;
-            for (int i = 0; i < sLength; ++i) {
-                char c = s.charAt(i);
-                int idx = c - 'a';
-                TrieNode child = current.children[idx];
-                if (child == null) {
-                    child = new TrieNode(c);
-                    current.children[idx] = child;
+        int minStrLen = minStr.length();
+        int idx = 0;
+        for (int i = 0; i < minStrLen; i++) {
+            char curr = minStr.charAt(i);
+            for (String str : strs) {
+                if (str.charAt(idx) != curr) {
+                    return minStr.substring(0, idx);
                 }
-                if (child.matches != matches) {
-                    break;
-                }
-                ++count;
-                ++child.matches;
-                current = child;
             }
-            return count;
+            ++idx;
         }
-    }
-
-    private static class TrieNode {
-        final char val;
-        int matches;
-        TrieNode[] children = new TrieNode[26];
-
-        TrieNode(char val) {
-            this.val = val;
-        }
+        return minStr.substring(0, idx);
     }
 }
