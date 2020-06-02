be6ed3daad4d40cbfb1f05761d34f53fc731fc88
miguelsalto <miguel.salto@gmail.com>
2020-06-01 19:20:27 -0700

    Simplify implementation of FirstUniqueCharacterInString

--- src/main/java/easy/FirstUniqueCharacterInString.java
+++ src/main/java/easy/FirstUniqueCharacterInString.java
@@ -1,25 +1,14 @@
 package easy;
 
-import java.util.Arrays;
-
 public class FirstUniqueCharacterInString {
     public int firstUniqChar(String s) {
-        int[] flags = new int[26];
-        Arrays.fill(flags, -2);
+        int[] counters = new int[26];
         int sLength = s.length();
         for (int i = 0; i < sLength; ++i) {
-            char c = s.charAt(i);
-            int idx = c - 'a';
-            if (flags[idx] == -2) {
-                flags[idx] = idx;
-            } else if (flags[idx] > -1) {
-                flags[idx] = -1;
-            }
+            ++counters[s.charAt(i) - 'a'];
         }
         for (int i = 0; i < sLength; ++i) {
-            char c = s.charAt(i);
-            int idx = c - 'a';
-            if (flags[idx] > -1) {
+            if (counters[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
